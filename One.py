d8ce06f07cc601575cb3b95d622bc44b8b136f20
Prabhnoor Singh <pslubana2004@gmail.com>
2022-10-10 14:22:47 +0530

    adding

---
+++ One.py
@@ -0,0 +1,18 @@
+x=5
+y=8
+print(x)
+a="This is a string"
+print(a)
+print(x,y)
+print(x+y)
+print(x-y)
+print(type(x))
+z=92
+n=float(z)
+print(type(n))
+b=x+y
+print(b)
+firstNumber=int(input("First Number is:"))
+secondNumber=int(input("Second Number is:"))
+sum=(firstNumber + secondNumber)
+print("The sum of two numbers is:",sum)
