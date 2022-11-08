94badb569b43c2fc46792262546b2d458026a62d
Prabhnoor Singh <pslubana2004@gmail.com>
2022-11-08 11:37:15 +0530

    adding

---
+++ bam.py
@@ -0,0 +1,7 @@
+marks=int(input("Enter the marks:"))
+if marks>90 and marks<=100:
+    print("Grade A")
+elif marks>80 and marks<=90:
+    print("Grade B")
+elif marks>70 and marks<=80:
+    print("Grade C")
