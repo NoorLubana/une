f9662db69d3fb824def370a020963acf7a24456b
Prabhnoor Singh <pslubana2004@gmail.com>
2022-11-08 11:21:08 +0530

    adding

--- ban.py
+++ ban.py
@@ -1,3 +1,6 @@
-num1=int(input("Number:"))
-sum=num1**0.5
-print(sum)
+a=float(input("first side:"))
+b=float(input("Second side:"))
+c=float(input("third side:"))
+s=(a+b+c)/2
+areaOfTriangle=(s*(s-a)*(s-b)*(s-c))**0.5
+print(areaOfTriangle)
