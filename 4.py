5e2fa88ca3708f1d15d91a2353c5f537b987eaa2
Prabhnoor Singh <pslubana2004@gmail.com>
2022-11-12 12:50:00 +0530

    adding

--- 4.py
+++ 4.py
@@ -1,2 +1,20 @@
-x= lambda num1, num2 : num1 + num2
-print(x(3,6))
+def add(a,b):
+    print(a+b)
+def sub(a,b):
+    print(a-b)
+def multi(a,b):
+    print(a*b)
+def divi(a,b):
+    print(a/b)
+
+a=int(input("First Number:"))
+b=int(input("Second Number:"))
+c=str(input("operator: "))
+if c=="+":
+    add(a,b)
+elif c=="-":
+    sub(a,b)
+elif c=="*":
+    multi(a,b)
+elif c=="/":
+    divi(a,b)
