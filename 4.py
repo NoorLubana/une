d81ed2bd998137066a62bd1dfe6b69ef137a12b5
Prabhnoor Singh <pslubana2004@gmail.com>
2022-11-13 15:22:54 +0530

    adding

--- 4.py
+++ 4.py
@@ -1,20 +1,3 @@
-def add(a,b):
-    print(a+b)
-def sub(a,b):
-    print(a-b)
-def multi(a,b):
-    print(a*b)
-def divi(a,b):
-    print(a/b)
-
-a=int(input("First Number:"))
-b=int(input("Second Number:"))
-c=str(input("operator: "))
-if c=="+":
-    add(a,b)
-elif c=="-":
-    sub(a,b)
-elif c=="*":
-    multi(a,b)
-elif c=="/":
-    divi(a,b)
+x=int(input("First Number:"))
+sqrroot=x**0.5
+print(sqrroot)
