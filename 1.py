0086f32b372567d0c836cc218eda3f6d8fffc751
Prabhnoor Singh <pslubana2004@gmail.com>
2022-11-13 15:17:12 +0530

    adding

--- 1.py
+++ 1.py
@@ -1,15 +1,6 @@
-def add(num1,num2):
-    sum=num1 + num2
-    print(sum)
-add(2,5)
-def subtract(num1,num2):
-    sub= num1 - num2
-    print(sub)
-subtract(2,5)
-
-def multiply(numn1,num2):
-    mult= numn1*num2
-    print(mult)
-multiply(3,89)
-
-
+x=5
+y=7.5
+print(x)
+print(y)
+print(x,y)
+print(x+y)
