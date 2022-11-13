27ead2d63305c27a9e597a6f85b66c9b1e16833c
Prabhnoor Singh <pslubana2004@gmail.com>
2022-11-13 15:44:07 +0530

    adding

---
+++ 9.py
@@ -0,0 +1,7 @@
+marks=int(input("Marks scored:"))
+if marks>90 and marks<=100:
+    print("Your grade is A")
+if marks>80 and marks<=90:
+    print("Your grade is B")
+if marks>70 and marks<=80:
+    print("Your grade is C")
