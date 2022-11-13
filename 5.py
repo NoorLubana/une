92c91e4321ebfd575f6f480a91654d3c7d0b980c
Prabhnoor Singh <pslubana2004@gmail.com>
2022-11-13 15:27:43 +0530

    adding

--- 5.py
+++ 5.py
@@ -1,2 +1,6 @@
-colors=["Red","Blue","Black"]
-print(colors)
+a=int(input("First side:"))
+b=int(input("Second side:"))
+c=int(input("Third side:"))
+s= (a+b+c)/2
+area=(s*(s-a)*(s-b)*(s-c))**0.5
+print(area)
