c7cc0176d712f8e709d45ae172d844de010552ed
Prabhnoor Singh <pslubana2004@gmail.com>
2022-11-25 15:51:40 +0530

    adding

---
+++ 32.py
@@ -0,0 +1,11 @@
+def hello():
+    print("hello world")
+hello()
+def add(num1,num2):
+    sum=num1 + num2
+    print(sum)
+add(2,7)
+def subtract(num1,num2):
+    sub=num1 - num2
+    print(sub)
+subtract(3,6)
