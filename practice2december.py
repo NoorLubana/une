ec1db711a4e63734accdc18cd24841963eb5cc40
Prabhnoor Singh <pslubana2004@gmail.com>
2022-12-02 11:39:43 +0530

    adding

---
+++ practice2december.py
@@ -0,0 +1,13 @@
+#userage=[21,22,34,45,67]
+#print(userage[2])
+#print(userage[0])
+#print(userage[-1])
+#print(userage[2:4])
+#print(userage[1:5:2])
+age=[32,45,75,43,22]
+age[2]=5
+print(age)
+age.append(99)
+print(age)
+del age[2]
+print(age)
