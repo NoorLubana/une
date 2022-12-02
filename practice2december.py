297945154b5bac406200522c35db6459339850d6
Prabhnoor Singh <pslubana2004@gmail.com>
2022-12-02 12:06:24 +0530

    adding

--- practice2december.py
+++ practice2december.py
@@ -4,10 +4,25 @@
 #print(userage[-1])
 #print(userage[2:4])
 #print(userage[1:5:2])
-age=[32,45,75,43,22]
-age[2]=5
-print(age)
-age.append(99)
-print(age)
-del age[2]
-print(age)
+
+#age=[32,45,75,43,22]
+#age[2]=5
+#print(age)
+#age.append(99)
+#print(age)
+#del age[2]
+#print(age)
+
+#tuple1=["jan","feb","wed","thur","fri","sat","sun"]
+#print(tuple1[1])
+
+#mydictionary={"peter":38, "john":51, "pet":43}
+#print(mydictionary["peter"])
+#del mydictionary["peter"]
+#print(mydictionary)
+
+#print('''Hello world.
+ #My name is James and
+# I am 20 years old.''')
+
+print('hello/tworld')
